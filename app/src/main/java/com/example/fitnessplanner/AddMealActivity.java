d889238618e87520ef57bd155b48a050669aa6c2
ap16at <ap16at@my.fsu.edu>
2021-07-05 22:32:39 -0400

    Working Progress bars Connected to Firebase Recycler View is not working

--- app/src/main/java/com/example/fitnessplanner/AddMealActivity.java
+++ app/src/main/java/com/example/fitnessplanner/AddMealActivity.java
@@ -1,14 +1,328 @@
 package com.example.fitnessplanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.fitnessplanner.models.Meal;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 public class AddMealActivity extends AppCompatActivity {
+
+    ProgressBar protein_bar;
+    int protein_progress;
+    TextView protein_bar_text;
+    ProgressBar carb_bar;
+    int carb_progress;
+    TextView carb_bar_text;
+    ProgressBar fat_bar;
+    int fat_progress;
+    TextView fat_bar_text;
+    ProgressBar total_cal_bar;
+    int total_progress;
+    TextView total_bar_text;
+
+    TextView description_label;
+    TextView size_label;
+    TextView calorie_label;
+    TextView protein_label;
+    TextView carbs_label;
+    TextView fat_label;
+
+    EditText description_field;
+    EditText size_field;
+    EditText calorie_field;
+    EditText protein_field;
+    EditText carbs_field;
+    EditText fat_field;
+
+    Button cancel_button;
+    Button add_button;
+
+    RecyclerView meals;
+    ColorStateList defaultColor;
+
+    FirebaseDatabase database;
+    DatabaseReference mealRef;
+
+    // sets textView colors to black
+    private void setColorDefault(){
+        description_label.setTextColor(defaultColor);
+        size_label.setTextColor(defaultColor);
+        calorie_label.setTextColor(defaultColor);
+        protein_label.setTextColor(defaultColor);
+        carbs_label.setTextColor(defaultColor);
+        fat_label.setTextColor(defaultColor);
+    }
+
+    // sets textView color to red
+    private void setColorRed(TextView tv){
+        tv.setTextColor(Color.parseColor("#FF0000"));
+    }
+
+    private void reset(){
+        description_field.setText("");
+        size_field.setText("");
+        calorie_field.setText("");
+        protein_field.setText("");
+        carbs_field.setText("");
+        fat_field.setText("");
+    }
+
+    private final View.OnClickListener cancelListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(AddMealActivity.this, MainActivity.class));
+        }
+    };
+
+    private final View.OnClickListener addListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+
+            String des = description_field.getText().toString();
+            int desLength = description_field.getText().length();
+            int sizeLength = size_field.getText().length();
+            int calLength = calorie_field.getText().length();
+            int proteinLength = protein_field.getText().length();
+            int carbsLength = carbs_field.getText().length();
+            int fatLength = fat_field.getText().length();
+
+            boolean addWorthy = true;
+
+            int size = 0;
+            int cal = 0;
+            int protein = 0;
+            int carbs = 0;
+            int fat = 0;
+
+            try{
+                size = Integer.parseInt(String.valueOf(size_field.getText()));
+            }catch(NumberFormatException ex){
+                setColorRed(size_label);
+            }
+
+            try{
+                cal = Integer.parseInt(String.valueOf(calorie_field.getText()));
+            }catch(NumberFormatException ex){
+                setColorRed(calorie_label);
+            }
+
+            try{
+                protein = Integer.parseInt(String.valueOf(protein_field.getText()));
+            }catch(NumberFormatException ex){
+                setColorRed(protein_label);
+            }
+
+            try{
+                carbs = Integer.parseInt(String.valueOf(carbs_field.getText()));
+            }catch(NumberFormatException ex){
+                setColorRed(carbs_label);
+            }
+
+            try{
+                fat = Integer.parseInt(String.valueOf(fat_field.getText()));
+            }catch(NumberFormatException ex){
+                setColorRed(fat_label);
+            }
+
+            if(desLength == 0){
+                setColorRed(description_label);
+                addWorthy = false;
+            }
+            if(sizeLength <= 0){
+                setColorRed(size_label);
+                addWorthy = false;
+            }
+            if(calLength <= 0){
+                setColorRed(calorie_label);
+                addWorthy = false;
+            }
+            if(proteinLength <= 0){
+                setColorRed(protein_label);
+                addWorthy = false;
+            }
+            if(carbsLength <= 0){
+                setColorRed(carbs_label);
+                addWorthy = false;
+            }
+            if(fatLength <= 0){
+                setColorRed(fat_label);
+                addWorthy = false;
+            }
+
+            if(addWorthy){
+                setColorDefault();
+                int finalSize = size;
+                int finalCal = cal;
+                int finalProtein = protein;
+                int finalCarbs = carbs;
+                int finalFat = fat;
+                mealRef.orderByChild("Meals").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        long count = snapshot.getChildrenCount();
+                        String meal_num = String.valueOf(++count);
+
+                        setColorDefault();
+                        Meal newMeal = new Meal(des, finalSize, finalCal, finalProtein, finalCarbs, finalFat);
+                        mealRef.child(meal_num).setValue(newMeal);
+
+                        View.OnClickListener snackActionListener = new View.OnClickListener(){
+                            @Override
+                            public void onClick(View v) {
+                                reset();
+                            }
+                        };
+
+                        update_progress_bars();
+                        Toast.makeText(AddMealActivity.this, "Meal Successfully Added!", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+            else
+            {
+                Toast.makeText(AddMealActivity.this, "Error: Some fields were empty.", Toast.LENGTH_LONG).show();
+            }
+
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_meal);
+
+        protein_bar = findViewById(R.id.protein_progress_bar);
+        carb_bar = findViewById(R.id.carb_progress_bar);
+        fat_bar = findViewById(R.id.fat_progress_bar);
+        total_cal_bar = findViewById(R.id.full_calorie_progress_bar);
+
+        protein_bar_text = findViewById(R.id.protein_progress_text);
+        carb_bar_text = findViewById(R.id.carb_progress_text);
+        fat_bar_text = findViewById(R.id.fat_progress_text);
+        total_bar_text = findViewById(R.id.full_calorie_progress_text);
+
+        description_label = findViewById(R.id.descriptiontv);
+        size_label = findViewById(R.id.sizetv);
+        calorie_label = findViewById(R.id.calorietv);
+        protein_label = findViewById(R.id.proteintv);
+        carbs_label = findViewById(R.id.carbtv);
+        fat_label = findViewById(R.id.fattv);
+        description_field = findViewById(R.id.descriptionet);
+        size_field = findViewById(R.id.sizeet);
+        calorie_field = findViewById(R.id.calorieet);
+        protein_field = findViewById(R.id.proteinet);
+        carbs_field = findViewById(R.id.carbet);
+        fat_field = findViewById(R.id.fatet);
+        cancel_button = findViewById(R.id.meal_cancel);
+        add_button = findViewById(R.id.meal_add);
+        meals = findViewById(R.id.rv_meals);
+
+        defaultColor = description_label.getTextColors();
+
+        cancel_button.setOnClickListener(cancelListener);
+        add_button.setOnClickListener(addListener);
+
+        database = FirebaseDatabase.getInstance();
+        mealRef = database.getReference("Meals");
+
+        update_progress_bars();
+
     }
+
+    private void update_progress_bars(){
+
+        //TODO: Allow user to input calorie goals to replace these temporary ones:
+
+        int totCalTemp = 2200;
+        int proTemp = 220;
+        int carbTemp = 193;
+        int fatTemp = 61;
+
+        mealRef.orderByChild("Meals").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                int total_sum = 0;
+                int protein_sum = 0;
+                int carb_sum = 0;
+                int fat_sum = 0;
+                for(DataSnapshot ds : snapshot.getChildren()){
+
+                    Map<String, Object>map = (Map<String,Object>) ds.getValue();
+                    Object calories = map.get("totalCalories");
+                    Object protein = map.get("protein");
+                    Object carbs = map.get("carbs");
+                    Object fat = map.get("fat");
+                    int tValue = Integer.parseInt(String.valueOf(calories));
+                    int pValue = Integer.parseInt(String.valueOf(protein));
+                    int cValue = Integer.parseInt(String.valueOf(carbs));
+                    int fValue = Integer.parseInt(String.valueOf(fat));
+                    total_sum += tValue;
+                    protein_sum += pValue;
+                    carb_sum += cValue;
+                    fat_sum += fValue;
+
+                }
+                total_progress = (total_sum*100)/totCalTemp;
+                protein_progress = (protein_sum*100)/proTemp;
+                carb_progress = (carb_sum*100)/carbTemp;
+                fat_progress = (fat_sum*100)/fatTemp;
+
+                if(total_progress > 100)
+                    total_progress = 100;
+                if(protein_progress > 100)
+                    protein_progress = 100;
+                if(carb_progress > 100)
+                    carb_progress = 100;
+                if(fat_progress > 100)
+                    fat_progress = 100;
+
+                total_bar_text.setText(String.valueOf(total_sum));
+                protein_bar_text.setText(String.valueOf(protein_sum));
+                carb_bar_text.setText(String.valueOf(carb_sum));
+                fat_bar_text.setText(String.valueOf(fat_sum));
+
+                protein_bar.setProgress(protein_progress);
+                carb_bar.setProgress(carb_progress);
+                fat_bar.setProgress(fat_progress);
+                total_cal_bar.setProgress(total_progress);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/fitnessplanner/HomeFragment.java
+++ app/src/main/java/com/example/fitnessplanner/HomeFragment.java
@@ -1,37 +1,59 @@
 package com.example.fitnessplanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import com.example.fitnessplanner.adapters.MealsAdapter;
+import com.example.fitnessplanner.models.Meal;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
     //TODO: Progress bar stuff
+    int water_progress = 0;
+    int calorie_progress = 0;
+    int workout_progress = 0;
+    private ProgressBar water_progress_bar;
+    private ProgressBar calorie_progress_bar;
+    private ProgressBar workout_progress_bar;
+    private Button button_increase;
+    private Button button_decrease;
+    private Button add_a_meal;
+    private RecyclerView meals_display;
+    private ArrayList<Meal> mealItems;
+    private ArrayList<Meal> tempList;
 
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FirebaseDatabase database;
+    DatabaseReference mealRef;
+    DatabaseReference specificMealRef;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -39,16 +61,158 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+//        if (getArguments() != null) {}
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        water_progress_bar = view.findViewById(R.id.water_progress_bar);
+        calorie_progress_bar = view.findViewById(R.id.calorie_progress_bar);
+        workout_progress_bar = view.findViewById(R.id.workout_progress_bar);
+
+        button_decrease = view.findViewById(R.id.water_decrease_button);
+        button_increase = view.findViewById(R.id.water_increase_button);
+
+        add_a_meal = view.findViewById(R.id.button_add_meal);
+        meals_display = view.findViewById(R.id.meals_display);
+
+        water_bar_update();
+
+        button_decrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(water_progress >= 10){
+                    water_progress -= 10;
+                    water_bar_update();
+                }
+            }
+        });
+
+        button_increase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(water_progress <= 90){
+                    water_progress += 10;
+                    water_bar_update();
+                }
+            }
+        });
+
+        add_a_meal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), AddMealActivity.class));
+            }
+        });
+
+        database = FirebaseDatabase.getInstance();
+        mealRef = database.getReference("Meals");
+        calorie_bar_update();
+
+        mealItems = new ArrayList<>();
+
+        mealRef.orderByChild("Meals").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                long count = snapshot.getChildrenCount();
+
+                ArrayList<Meal> tempList = new ArrayList<>();
+
+                for(int i = 1; i <= (int)count; i++){
+                    mealRef.child(String.valueOf(i)).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            String description = snapshot.child("description").getValue(String.class);
+                            int size = snapshot.child("servingSize").getValue(Integer.class);
+                            int calories = snapshot.child("totalCalories").getValue(Integer.class);
+                            int protein = snapshot.child("protein").getValue(Integer.class);
+                            int carbs = snapshot.child("carbs").getValue(Integer.class);
+                            int fat = snapshot.child("fat").getValue(Integer.class);
+
+                            System.out.println("des: " + description + " calories: " + calories);
+
+                            mealItems.add(new Meal(description, size, calories, protein, carbs, fat));
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+
+                setRecyclerView(view);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return view;
+
     }
+
+    private void setRecyclerView(View v){
+
+        meals_display = v.findViewById(R.id.meals_display);
+        meals_display.setLayoutManager(new LinearLayoutManager(getActivity()));
+        MealsAdapter mealsAdapter = new MealsAdapter(getActivity(), mealItems);
+        meals_display.setAdapter(mealsAdapter);
+        meals_display.setItemAnimator(new DefaultItemAnimator());
+
+    }
+
+    void water_bar_update(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                water_progress_bar.setProgress(water_progress);
+            }
+        }, 50);
+    }
+
+    private void calorie_bar_update(){
+
+        //TODO: Allow user to input calorie goals to replace these temporary ones:
+        int totCalTemp = 2200;
+
+        mealRef.orderByChild("Meals").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                int total_sum = 0;
+                for(DataSnapshot ds : snapshot.getChildren()){
+
+                    Map<String, Object> map = (Map<String,Object>) ds.getValue();
+                    Object calories = map.get("totalCalories");
+                    int tValue = Integer.parseInt(String.valueOf(calories));
+                    total_sum += tValue;
+
+                }
+                calorie_progress = (total_sum*100)/totCalTemp;
+
+                if(calorie_progress > 100)
+                    calorie_progress = 100;
+
+                calorie_progress_bar.setProgress(calorie_progress);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }
